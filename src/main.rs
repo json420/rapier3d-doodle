07b5720afd3f507072808546b4d7e27e784d0256
Jason Gerard DeRose <jasonderose@gmail.com>
2026-09-03 23:35:39 -0600

    setup and spawn

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,23 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugins(RapierDebugRenderPlugin::default())
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-3.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+
+    commands
+        .spawn(Collider::cuboid(100.0, 0.1, 100.0))
+        .insert(Transform::from_xyz(0.0, -2.0, 0.0));
+
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(0.5))
+        .insert(Restitution::coefficient(0.7))
+        .insert(Transform::from_xyz(0.0, 4.0, 0.0));
 }
