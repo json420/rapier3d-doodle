8ffc0974aba2db5883bc5ea8553b3c4c720d11bc
Jason Gerard DeRose <jasonderose@gmail.com>
2026-09-03 23:05:54 -0600

    now with plugins

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin::default())
+        .run();
 }
